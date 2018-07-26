c7083a86c33556cb77ddce832e2555f8ad5453f3
Alesya Kovalevich <AlesyaKo@hexagontech.co.il>
2018-07-26 15:10:29 +0300

    init rep

---
+++ QATestTask/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PageObjects.WebDriver;
+using PageObjects;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace QATestTask
+{
+    [TestClass]
+    public class Tests
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# qa-test-task
+There are two sets of duplicate test methods.
+Tests.cs - test class that runs its test cases one by one, closing browser each time.
+TestsSingleSetup.cs - test class that opens browser, runs all cases and closes browser afterwards.
